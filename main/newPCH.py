7a44b2f0e2fa1dae230eabe80ac74179e52d86cc
XYSummerT <63708224+XYSummerT@users.noreply.github.com>
2021-03-14 21:05:11 +0800

    主体用于代替旧的PCH

---
+++ main/newPCH.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python 
+
+# -*- coding: utf-8 -*-
+
+
+
+#导入模块
+import PCalculator
+import Trajectory
+
+
+
+
+#输入值
+#px = float(input("请输入珍珠坐标x"))
+#pz = float(input("请输入珍珠坐标z"))
+#dx = float(input("请输入目的地坐标x"))
+#dz = float(input("请输入目的地坐标z"))
+#dtnt = float(input("请输入tnt加速度"))
+#gt = float(input("请输入到达时间"))
+
+mx = 100.0
+mz = 100.0
+dtnt = 0.6025
+gt = 1.0
+px = 0.0
+pz = px
+
+
+
+
+
+#计算 比较
+#mx = dx - px
+#mz = dz - pz
+
+
+
+if mx > 0:
+    if mz > 0:
+        tntx,tntz,mx,mz = PCalculator.pearl(mx,mz,gt,dtnt)
+    else:
+        mz = mz * (-1)
+        tntx,tntz,mx,mz = PCalculator.pearl(mx,mz,gt,dtnt)
+else:
+    if mz > 0:
+        tntx,tntz,mx,mz = PCalculator.pearl(mx,mz,gt,dtnt)
+    else:
+        mz = mz * (-1)
+        tntx,tntz,mx,mz = PCalculator.pearl(mx,mz,gt,dtnt)
+
+
+
+#输出结果
+print(str(tntx)+" "+ str(tntz))
+
+
+
+#计算弹道？
+
+
+time = 0
+dx = px + mx
+dz = pz + mz
+print(dx,dz)
+while time != 10:
+    mx,mz,dx,dz = Trajectory.Trajectory(mx,mz,dx,dz)
+    time = time+1
+    print(dx,dz)
+
